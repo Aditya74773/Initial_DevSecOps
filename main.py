f69598e3a9b2e23a81736849453b3058e0e1070a
Aditya74773 <adityakum2004@gmail.com>
2025-08-02 10:37:40 +0530

    main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return {"message": "Welcome to DevSecOps!"}
+@app.route("/")
+def hello():
+    return "Hello from Flask inside Docker DevSecOps!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
